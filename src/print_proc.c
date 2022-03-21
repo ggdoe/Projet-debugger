d7386943ca7dfe382f1b90d5a0d5ffd15c6731c5
ggdoe <gregoire.doebele@ens.uvsq.fr>
2022-03-21 23:49:04 +0100

    amélioration explorateur de /proc/child

--- src/print_proc.c
+++ src/print_proc.c
@@ -45,21 +45,21 @@ void print_maps(){
 void explore_proc()
 {
 	struct dirent ** cur_dir = NULL;
-	int nb_file;
+	size_t nb_file;
 	char path[256];
 	char to_open[256];
 	size_t nbr_match = 0;
-	unsigned char file_type;
+	size_t index_match;
+	bool root_proc = true; // est ce qu'on est dans le dossier /proc/child/
 
 	snprintf(path, 256, "/proc/%d/", child);
 
 	scan_dir:
 	nb_file = scandir(path, &cur_dir, NULL, alphasort);
 
-	ask_dir:
-
+	printf("\n\033[33m%-17s", "q : retour");
 	// On print les fichiers (sauf les 2 premiers : . et ..)
-	for(int i = 2; i < nb_file; i++){
+	for(size_t i = 2; i < nb_file; i++){
 		switch (cur_dir[i]->d_type){
 			case DT_DIR: // current entry is a DIR
 				printf("\033[95m"); break;
@@ -79,46 +79,64 @@ void explore_proc()
 	printf("\033[0m");
 	
 	// 'q' pour quiter
-	if(to_open[0] == 'q' && (to_open[1] == '\0' || to_open[1] == '\n'))
+	if(to_open[0] == 'q' && to_open[1] == '\0'){
+		// si on est pas dans /proc/child
+		if(!root_proc){
+			root_proc = true;
+			snprintf(path, 256, "/proc/%d", child); // on revient sur /proc
+			goto scan_dir;
+		}
+		// sinon
 		goto end; 
+	}
 
 	// On parcours le dossier pour voir qui match la recherche
 	nbr_match = 0;
-	for(int i = 2; i < nb_file; i++){
-		const char *current_file = cur_dir[i]->d_name;
-		size_t cursor = 0;
-		bool match = true;
+	int cursor_max = 0;
+	for(size_t i = 2; i < nb_file; i++){
+		int cursor = 0;
 
-		while(current_file[cursor] != '\0' || to_open[cursor] != '\0'){
-			if(to_open[cursor] != current_file[cursor]){
-				match = false;
-				break;
-			}
+		// On compte le nombre de caractère identique dont \0
+		while(to_open[cursor] == cur_dir[i]->d_name[cursor]){
 			cursor++;
 		}
-		if(match){
-			char* dup_path = strdup(path);
-			snprintf(path, 256, "%s/%s", dup_path, current_file); // on met à jour le path avec le dossier choisi (ici car on aura plus accès à 'current_file' en dehors de la boucle)
-			file_type = cur_dir[i]->d_type; // on enregistre le type de fichier
+
+		// on compte le nombre de match pour une longueur donnée
+		if(cursor == cursor_max)
 			nbr_match++;
-			free(dup_path);
+		else if(cursor > cursor_max){
+			nbr_match = 1; // si on atteint un nouveau max, nbr match = 1
+			cursor_max = cursor;
+			index_match = i;
 		}
 	}
+	
 	// Si il a plusieurs match, on redemande
 	if(nbr_match != 1){
-		printf("%ld\n", nbr_match);
-		printf("\n\033[31mpath incorrect.\n");
-		goto ask_dir;
+		if(nbr_match == nb_file - 2) nbr_match = 0; // on n'a pas bouclé sur les fichier . et ..
+		printf("\n\033[31mpath incorrect. (%ld correspondance)\n", nbr_match);
+		goto scan_dir;
 	}
-
 	printf("\n");
-	if(file_type != DT_DIR){ // si le path n'est pas un dossier
+
+	// on concatene le path et le fichier/dossier choisi
+	char* dup_path = strdup(path);
+	snprintf(path, 256, "%s/%s", dup_path, cur_dir[index_match]->d_name); // on met à jour le path avec le dossier choisi (ici car on aura plus accès à 'current_file' en dehors de la boucle)
+	
+	 // si le path n'est pas un dossier
+	if(cur_dir[index_match]->d_type != DT_DIR){
 		print_file(path); // on le print
+		strcpy(path, dup_path); // on se replace dans le dossier
 		printf("\n\n");
-		snprintf(path, 256, "/proc/%d", child); // et on revient sur /proc
+		// snprintf(path, 256, "/proc/%d", child); // et on revient sur /proc
 	}
+	else 
+		root_proc = false; // on s'est déplacé dans un dossier
+
+	free(dup_path);
 	goto scan_dir;
 
+	//
 	end:
 	printf("\033[0m\n");
 	free(cur_dir);
